3f75aef3e8a3ad47ebd4538fec2a78751336a4ab
한경만 <kmhan@spectra.co.kr>
2024-10-30 06:53:21 +0900

    update

---
+++ 05_langgraph_course/graph/nodes/retrieve.py
@@ -0,0 +1,14 @@
+from typing import Dict, Any
+
+
+from graph.state import GraphState
+from ingestion import retriever
+
+
+def retrieve(state: GraphState) -> Dict[str, Any]:
+    print("---RETRIEVE---")
+    question = state["question"]
+
+    documents = retriever.invoke(question)
+
+    return {"documents": documents, "question": question}
---
+++ 05_langgraph_course/graph/state.py
@@ -0,0 +1,18 @@
+from typing import TypedDict, List
+
+
+class GraphState(TypedDict):
+    """
+    Represents the state of out Graph.
+
+    Attributes:
+        question: question
+        generation: LLM generation
+        web_search: whether to add search
+        documents: list of documents
+    """
+
+    question: str
+    generation: str
+    web_search: bool
+    documents: List[str]
