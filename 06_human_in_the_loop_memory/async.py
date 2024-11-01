2525e658a5565c93c8bd2c705e19aa5dcaddd876
rudaks <rudaks94@gmail.com>
2024-11-01 20:09:44 +0900

    update

--- 06_human_in_the_loop_memory/async.py
+++ 06_human_in_the_loop_memory/async.py
@@ -1,5 +1,5 @@
 import operator
-from typing import TypedDict, Annotated, Any
+from typing import TypedDict, Annotated, Any, Sequence
 
 from langgraph.constants import START, END
 from langgraph.graph import StateGraph
@@ -7,6 +7,7 @@ from langgraph.graph import StateGraph
 
 class State(TypedDict):
     aggregate: Annotated[list, operator.add]
+    which: str
 
 
 class ReturnNodeValue:
@@ -25,17 +26,28 @@ builder = StateGraph(State)
 builder.add_node("a", ReturnNodeValue("I'm A"))
 builder.add_edge(START, "a")
 builder.add_node("b", ReturnNodeValue("I'm B"))
-builder.add_node("b2", ReturnNodeValue("I'm B2"))
 builder.add_node("c", ReturnNodeValue("I'm C"))
 builder.add_node("d", ReturnNodeValue("I'm D"))
-builder.add_edge("a", "b")
-builder.add_edge("a", "c")
-builder.add_edge("b", "b2")
-builder.add_edge(["b2", "c"], "d")
-builder.add_edge("d", END)
+builder.add_node("e", ReturnNodeValue("I'm E"))
+
+
+def route_bc_or_cd(state: State) -> Sequence[str]:
+    if state["which"] == "cd":
+        return ["c", "d"]
+    return ["b", "c"]
+
+
+intermediates = ["b", "c", "d"]
+builder.add_conditional_edges("a", route_bc_or_cd, intermediates)
+for node in intermediates:
+    builder.add_edge(node, "e")
+builder.add_edge("e", END)
+
 graph = builder.compile()
 graph.get_graph().draw_mermaid_png(output_file_path="async.png")
 
 if __name__ == "__main__":
     print("Hello Async Graph")
-    graph.invoke({"aggregate": []}, {"configurable": {"thread_id": "foo"}})
+    graph.invoke(
+        {"aggregate": [], "which": "cd"}, {"configurable": {"thread_id": "foo"}}
+    )
